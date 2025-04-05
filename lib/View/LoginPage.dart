93e304dc17112874b33ec7f31aa4576020ff0a46
MRD-HG <roudani.mustapha0@gmail.com>
2025-04-05 22:05:59 +0000

    build leading page

---
+++ lib/View/LoginPage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+class Loginpage extends StatefulWidget {
+  const Loginpage({super.key});
+
+  @override
+  State<Loginpage> createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+    );
+  }
+}
+class Buttons extends StatelessWidget {
+  const Buttons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
